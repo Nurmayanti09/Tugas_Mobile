f0d1abbf7ffc94603db227616c9e47045ec5d5a9
Nurmayanti09 <99806172+Nurmayanti09@users.noreply.github.com>
2022-04-21 22:39:00 +0800

    Add files via upload

---
+++ IMAGE CLASS SOURCODE.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('NURMAYANTI_D0219364'),
+      ),
+      body: Center(
+        child: Image(image: AssetImage('assets/profile.png')),
+      ),
+    );
+  }
+}
